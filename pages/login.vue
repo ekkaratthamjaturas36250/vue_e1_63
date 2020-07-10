fd04c70e24b33af09791f6d03673f23df27866cb
ekkaratthamjaturas36250 <ita5935@ccollege.ac.th>
2020-07-10 14:26:13 +0700

    sent_login

--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,44 @@
 <template>
   <v-app>
-    <v-card>
-      <v-card-title>
-        <h1>Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" prepend-icon="account_circle" />
-          <v-text-field type="password" label="Password" prepend-icon="mdi-lock" append-icon="visibility_off" />
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="warning">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="success">Login</v-btn>
-      </v-card-actions>
+    <center>
+      <v-card width="420" color="#708090">
+        <v-card-title>
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="account_circle" />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="warning">
+            Register
+          </v-btn>
+          <v-spacer />
+          <v-btn color="success">
+            Login
+          </v-btn>
+        </v-card-actions>
       </v-card>
+    </center>
+    </v-card>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
